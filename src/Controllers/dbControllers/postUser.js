57af173c78bb9723f30882ee7ea54befbc8cdc27
Carla Vega <109261938+carlavega13@users.noreply.github.com>
2023-09-19 20:03:04 -0300

    commit

--- src/Controllers/dbControllers/postUser.js
+++ src/Controllers/dbControllers/postUser.js
@@ -3,6 +3,7 @@ const {User} = require("../../db");
 const postUser=async(user)=>{
 
 try {
+    console.log(user);
     if(typeof user.username==="string" && typeof user.password==="string"){
         const response=await User.create(user)
     
