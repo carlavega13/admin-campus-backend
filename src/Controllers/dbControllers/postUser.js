d882f5cd67fda6b64fa9a7108f2d25707891216b
Carla Vega <109261938+carlavega13@users.noreply.github.com>
2023-09-20 14:59:22 -0300

    listo deploy

--- src/Controllers/dbControllers/postUser.js
+++ src/Controllers/dbControllers/postUser.js
@@ -6,7 +6,7 @@ try {
     console.log(typeof user.username,typeof user.password,typeof user.rol, typeof user.isSuperAdmin);
     if(typeof user.username==="string" && typeof user.password==="string"){
         const response=await User.create(user)
-    
+    console.log(response);
         return response
     }
 } catch (error) {
--- src/db.js
+++ src/db.js
@@ -5,8 +5,8 @@ const path = require("path");
 const {DB_URL,DB_HOST,DB_PASSWORD,DB_USER} = process.env;
 
 const sequelize = new Sequelize(
-  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/admin_campus`,
-  `${DB_URL}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/admin_campus`,
+  // `${DB_URL}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
