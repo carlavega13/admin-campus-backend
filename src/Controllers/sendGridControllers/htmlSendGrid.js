d4f58194bbbd63e2dcfe61e36872f08033e99c07
carlavega13 <carlavega231323@gmail.com>
2024-01-31 12:43:57 -0300

    deploy

---
+++ src/Controllers/sendGridControllers/htmlSendGrid.js
@@ -0,0 +1,61 @@
+const mail = (text) => {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Correo Electrónico</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 20px auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #555;
+        }
+
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #007bff;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        .footer {
+            margin-top: 20px;
+            text-align: center;
+            color: #777;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div>
+       ${text}
+        </div>
+    </div>
+    <div class="footer">
+        <p>© 2024 Edutech <a href="#">visita nuestra pagina.</a></p>
+    </div>
+</body>
+</html>`;
+};
+module.exports = mail;
--- src/Controllers/sendGridControllers/postMailController.js
+++ src/Controllers/sendGridControllers/postMailController.js
@@ -1,8 +1,14 @@
 const sgMail = require("./sendGridSetApiKey");
+const mail=require("./htmlSendGrid")
 const postMailController = async (info) => {
   try {
+    console.log(info);
+    const mailToSend=mail(info.text)
+    // console.log(mailToSend);
     const msg = {
-      ...info,
+      subject:info.subject,
+      to:info.to,
+     html:mailToSend,
       from: "ad.campus.13@gmail.com",
     };
     const response = await sgMail.send(msg);
